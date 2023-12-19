2a2bb06e4baa65febe41ce0acafef95f092b0df4
Ahmed <alqossaba@gmail.com>
2023-12-18 21:02:02 -0500

    hello world

---
+++ hello_world/helloworld.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout<< "what is your name? ";
+
+    string name;
+    cin >> name;
+
+    cout << "Hello "  << name << "\n";
+
+    int x;
+    int y;
+
+    cout << "What would you want X to be?";
+    cin >> x;
+
+    cout << "what would you like Y to be?";
+    cin >> y;
+
+
+    int a =x + y;
+
+
+
+    cout  << x << " + " << y << " = " <<a;
+
+    return 0;
+}
