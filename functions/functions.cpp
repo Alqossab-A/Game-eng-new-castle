ddba250acb8cef32c71097d162c6a6d7ea89e123
Ahmed <alqossaba@gmail.com>
2023-12-19 23:13:39 -0500

    functions

---
+++ functions/functions.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+void add(int x, int y);
+
+int minus(int x, int y);
+
+int processEvents(function<int(int, int)> someEvent, int x, int y) {
+    return someEvent(x, y);
+}
+
+int main() {
+    function<int(int, int)> onAddEvent = [](int x, int y) {
+        return x + y;
+    };
+    
+    function<int(int, int)> onMinusEvent = [](int x, int y) {
+        return x - y;
+    };
+    
+    cout << "add: " << processEvents(onAddEvent, 69, 69) << endl;
+    cout << "subtract: " << processEvents(onMinusEvent, 69, 69) << endl;
+    
+    int a, b, c, exit;
+
+    cout << "Please type in a number" << endl;
+    cin >> a;
+ 
+    cout << "Please type in another number" << endl;
+    cin >> b;
+
+    //c = add(a, b);
+
+    //cout << a << " + " << b << " = " << c << endl;
+    //cout << a << " + " << b << " = " << add(a, b) << endl;
+    add(a, b);
+
+    cout << "enter another number to exit" << endl;
+
+    cin >> exit;
+
+    return 0;
+}
+
+int minus(int x, int y) {
+    return x - y;
+}
+
+void add(int x, int y) {
+    int answer = x + y;
+    cout << "equal: " << answer << endl;
+}
