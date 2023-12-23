11cc1acf02e15b959931f3c534e4a4b82d3d41f8
Ahmed <alqossaba@gmail.com>
2023-12-23 00:09:38 -0500

    program flow update

--- program_flow/programflow.cpp
+++ program_flow/programflow.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int add(int x, int y);
 int fac(int x);
+bool value(int x);
 
 int main() {
     int total = 0;
@@ -14,7 +15,9 @@ int main() {
 
     while (total <= 2000) {
         cin >> x;
+        value(x);
         total = add(fac(x), total);
+        cout << "total: " << total << endl;
     }
     
     cout << "The total is " << total << endl;
@@ -34,6 +37,7 @@ int fac(int x) {
 
     for (int count = x; count > 0; count--) {
         facSum = facSum + count;
+        cout << "facSum " << facSum << ",";
     }
 
     return facSum;
@@ -41,5 +45,15 @@ int fac(int x) {
 
 int add(int x, int y) {
     return x + y;
+};
+
+bool value(int x) {
+    if (x >= 0) {
+        cout << "true" << endl;
+        return true;
+    } else {
+        cout << "false" << endl;
+        return false;
+    }
 };
 
